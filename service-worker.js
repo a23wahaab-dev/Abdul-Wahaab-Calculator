db90d3afe861fc2b65f072ab843dba1d478b5077
a23wahaab-dev <a23wahaab@gmail.com>
2026-07-12 20:28:45 +0500

    Add files via upload
    A service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,11 @@
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
 
+self.addEventListener("activate", (event) => {
+    console.log("Activated");
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(fetch(event.request));
+});
